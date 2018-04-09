3561e19f656b47c0fdebbbc88827a0f37cda9da3
guyjurson <guyju70@hotmail.fr>
2018-04-09 14:41:17 +0200

    API

---
+++ API/test.php
@@ -0,0 +1,19 @@
+<?php
+ 
+$adresse = "http://www.marmiton.org/recettes/recherche.aspx?type=all&aqt=pomme-poire";
+$page = file_get_contents ($adresse);
+ 
+preg_match_all('#(<a  class="recipe-card" href=")(.*)">#', $page, $lien);
+
+preg_match_all('#(<h4 class="recipe-card__title">)(.*)</h4>#', $page, $titre);
+ 
+/*var_dump($lien[2]); // Le var_dump() du tableau $prix nous montre que $prix[0] contient l'ensemble du morceau trouvé et que $prix[1] contient le contenu de la parenthèse capturante
+var_dump($titre[2]);*/
+
+$i=0;
+while ($i<count($lien[2])){
+	if(strpos($lien[2][$i], "video/")==false){
+		echo "<ul>".$titre[2][$i]."<li> http://www.marmiton.org".$lien[2][$i]."</li></ul>";
+	}
+	$i+=1;
+}
