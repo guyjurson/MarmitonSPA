bc351be3c455492f2cacb0aa1b36f387236b7715
Paul Renoult <prenoult@MacBook-Pro-de-Paul-Renoult.local>
2018-04-16 14:55:47 +0200

    Nettoyage du code du Back-end

--- API/InfoRequest.php
+++ API/InfoRequest.php
@@ -1,8 +1,8 @@
 <?php
-$ad=$_GET['adr'];
- 
-$adresse = "http://www.marmiton.org/recettes/".$ad;
-$page = file_get_contents ($adresse);
+$ad = $_GET['adr'];
+
+$adresse = "http://www.marmiton.org/recettes/" . $ad;
+$page = file_get_contents($adresse);
 
 //Titre
 preg_match_all('#(<h1 class="main-title ">)(.*)(</h1>)#', $page, $titre);
@@ -20,20 +20,20 @@ preg_match_all('#(<span class="ingredient">)(.*)</span><span#', $page, $ing);
 preg_match_all('#(<h3 class="__secondary">)(.*)(</h3>)(.*)(</li>)#', $page, $rec);
 
 
-echo "Pour faire ".$titre[2][0]." pour ".$nbPer[2][0]." personnes, il vous faut:";
-$i=0;
-while ($i<count($qua[2])-3){
-	echo "<li>".$qua[2][$i]." ".$ing[2][$i]."</li>";
-	$i+=1;
+echo "Pour faire " . $titre[2][0] . " pour " . $nbPer[2][0] . " personnes, il vous faut:";
+$i = 0;
+while ($i < count($qua[2]) - 3) {
+    echo "<li>" . $qua[2][$i] . " " . $ing[2][$i] . "</li>";
+    $i += 1;
 }
-echo "</br></br>Le temps de préparation est de ".$temps[2][0].".</br> 
-La difficulté est ".$dif[2][2]." et la coût est ".$dif[2][3].".</br>";
+echo "</br></br>Le temps de préparation est de " . $temps[2][0] . ".</br> 
+La difficulté est " . $dif[2][2] . " et la coût est " . $dif[2][3] . ".</br>";
 
 echo "Pour effectuer cette recette, vous devez:";
-$i=0;
-while ($i<count($rec[2])){
-	echo "<li>".$rec[2][$i].": ".strip_tags($rec[4][$i])."</li>";
-	$i+=1;
+$i = 0;
+while ($i < count($rec[2])) {
+    echo "<li>" . $rec[2][$i] . ": " . strip_tags($rec[4][$i]) . "</li>";
+    $i += 1;
 }
 
 
--- API/RecetteRequest.php
+++ API/RecetteRequest.php
@@ -1,35 +1,19 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-//echo "test";
-$ingr="";
+$ingr = "";
 foreach ($_POST as $key => $v) {
-  $ingr= $ingr.$v."-";
+    $ingr = $ingr . $v . "-";
 }
-$ingr=substr($ingr, 0, -1);
-
-//$ingr=$_GET['aqt'];
- 
-$adresse = "http://www.marmiton.org/recettes/recherche.aspx?type=all&aqt=".$ingr;
-$page = file_get_contents ($adresse);
- 
+$ingr = substr($ingr, 0, -1);
+$adresse = "http://www.marmiton.org/recettes/recherche.aspx?type=all&aqt=" . $ingr;
+$page = file_get_contents($adresse);
 preg_match_all('#(<a  class="recipe-card" href=")(.*)">#', $page, $lien);
-
 preg_match_all('#(<h4 class="recipe-card__title">)(.*)</h4>#', $page, $titre);
- 
-/*var_dump($lien[2]); // Le var_dump() du tableau $prix nous montre que $prix[0] contient l'ensemble du morceau trouvé et que $prix[1] contient le contenu de la parenthèse capturante
-var_dump($titre[2]);*/
+$i = 0;
 
-$i=0;
-
-
-
-while ($i<count($lien[2])){
-	if(strpos($lien[2][$i], "video/")==false){
-		echo $adresse."<ul>".$titre[2][$i]."<li>".$lien[2][$i]."</li></ul>";
-	}
-	$i+=1;
+while ($i < count($lien[2])) {
+    if (strpos($lien[2][$i], "video/") == false) {
+        echo $adresse . "<ul>" . $titre[2][$i] . "<li>" . $lien[2][$i] . "</li></ul>";
+    }
+    $i += 1;
 }
-
-
-
-//
